0fff6d0d2d125bd12a49366cddca1cdece1580e8
Nathan Oyler <notque@gmail.com>
2017-12-05 10:12:21 -0700

    add config file, command line options for es and mq

--- main.go
+++ main.go
@@ -1,16 +1,64 @@
 package main
 
 import (
-	"github.com/notque/hermes-etl/sink"
-	"github.com/notque/hermes-etl/source"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/sapcc/hermes-etl/sink"
+	"github.com/sapcc/hermes-etl/source"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	// Handle Config options, command line support for config location
+	configPath := parseCmdFlags()
+	setDefaultConfig()
+	readConfig(configPath)
+
 	//URI := "amqp://guest:guest@localhost:5672/"
 	//fmt.Println(URI)
-	sinkconn := sink.Sink{URI: "http://127.0.0.1:9200"}
+	sinkconn := sink.Sink{URI: viper.GetString("elasticsearch.uri")}
 	sink := sinkconn.ConnectSink()
-	mqconn := source.Source{URI: "amqp://guest:guest@localhost:5672/", Queue: "hello"}
+	mqconn := source.Source{URI: viper.GetString("rabbitmq.uri"), Queue: viper.GetString("rabbitmq.queue")}
 	mqconn.ConnectSource(sink)
 
+}
+
+// parseCmdFlags grabs the location to hermes-etl.conf and parses it, or prints Usage
+func parseCmdFlags() *string {
+	// Get config file location
+	configPath := flag.String("f", "hermes-etl.conf", "specifies the location of the TOML-format configuration file")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+	return configPath
+}
+
+func setDefaultConfig() {
+	viper.SetDefault("elasticsearch.uri", "http://127.0.0.1:9200")
+	viper.SetDefault("rabbitmq.uri", "amqp://guest:guest@localhost:5672/")
+	viper.SetDefault("rabbitmq.queue", "test")
+}
+
+// readConfig reads the configuration file from the configPath
+func readConfig(configPath *string) {
+	// Don't read config file if the default config file isn't there,
+	//  as we will just fall back to config defaults in that case
+	var shouldReadConfig = true
+	if _, err := os.Stat(*configPath); os.IsNotExist(err) {
+		shouldReadConfig = *configPath != flag.Lookup("f").DefValue
+	}
+	// Now we sorted that out, read the config
+	fmt.Printf("Should read config: %v, config file is %s", shouldReadConfig, *configPath)
+	if shouldReadConfig {
+		viper.SetConfigFile(*configPath)
+		viper.SetConfigType("toml")
+		err := viper.ReadInConfig()
+		if err != nil { // Handle errors reading the config file
+			panic(fmt.Errorf("Fatal error config file: %s", err))
+		}
+	}
 }
---
+++ misc/mqsend.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+// Elastic search at 127.0.0.1
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	//body := "hello"
+	body, err := ioutil.ReadFile("cadf-example.json")
+	failOnError(err, "Could not Read File")
+	err = ch.Publish(
+		"",     //exchange
+		q.Name, // Routing Key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	log.Printf(" [x] Sent %s", body)
+	failOnError(err, "Failed to publish a message")
+}
+
+// Helper function for errors for each amqp call
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
